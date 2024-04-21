9fd815845e7b0d9309a7d0ce792fe68967f7d498
Prateek-rajput-007 <prateekrajput47@yahoo.com>
2024-04-22 01:57:21 +0530

    initial commit

---
+++ src/styles/index.ts
@@ -0,0 +1,7 @@
+export * from "./theme";
+export * from "./globalStyles";
+export * from "./home.styled";
+export * from "./addTask.styled";
+export * from "./keyframes.styled";
+export * from "./common.styled";
+export * from "./categories.styled";
